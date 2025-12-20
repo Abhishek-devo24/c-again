08516a2dc629a61121d3b6b9683a3e4b89dd86e9
Abhishek-devo24 <guptaabhishek27538@gmail.com>
2025-12-20 20:11:18 +0530

    20 dec update where file fully change

---
+++ Helloprogram/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Helloprogram
+{
+    public class Getdata
+    {
+        public static void Run()
+        {
+            Console.Write("Enter emp id: ");
+            int empid = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter emp name: ");
+            string empname = Console.ReadLine();
+
+            Console.Write("Enter emp salary: ");
+            double empsalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Emp ID={empid}, Name={empname}, Salary={empsalary}");
+            Console.ReadKey();
+        }
+    }
+}
--- Helloprogram/Program.cs
+++ Helloprogram/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
-
+namespace Helloprogram
+{
     class Program
     {
         static void Main(string[] args)
         {
-        //int empid = 21;
-        //string empname = "lala ji";
-        //float empsalary = 25000.12f;   // for float, we use the f to determine the decimal value written in float
-        //Console.WriteLine("emp id=" + empid);
-        //Console.WriteLine("emp name=" + empname);
-        //Console.WriteLine("emp salary=" + empsalary);
-        //Console.WriteLine("hello bro");
-        int empid;
-        string empname;
-        double empsalary;
-        Console.WriteLine("enter emp id,");
-        empid = int.Parse(Console.ReadLine()); //for insert value from user we need to be use console.readline
-        Console.WriteLine("enter emp name");
-        empname = Console.ReadLine(); //syntax for string value from userdeclaration of variable = console.readline();
-        Console.WriteLine("enter emp salary");
-        empsalary = double.Parse(Console.ReadLine());  //for float / int value from user we need to use double.parse(console.readline()); or int.parse(console.readline());
-        Console.WriteLine($"emp id={empid},empname={empname},empsalary{empsalary}" );
-        Console.ReadKey(); // to hold the output screen
+            Console.WriteLine("1= Getdata");
+            Console.WriteLine("2= operator");
+            Console.WriteLine("3= loops");
+
+            int find = int.Parse(Console.ReadLine());
+
+            switch (find)
+            {
+                case 1:
+                    Getdata.Run();
+                    break;
+                default:
+                    Console.WriteLine("invalid input"); break;
+            }
         }
+
     }
+}
