240d0f333ae796d54a8624b641ba302e9492e337
Abhishek-devo24 <guptaabhishek27538@gmail.com>
2025-12-24 19:47:03 +0530

    Assignment operator

--- Helloprogram/Binary_operator.cs
+++ Helloprogram/Binary_operator.cs
@@ -11,23 +11,38 @@ namespace Helloprogram
             //int a = 21, b = 13;
             //int c;
             //c = a + b;
-            //Console.WriteLine("value of a and b="+c);
-            //Console.ReadKey();
             Console.WriteLine("value of a");
-            int a=int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
             Console.WriteLine("value of b");
-            int b=int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
             int c;
-            c= a + b; //addition
-            Console.WriteLine("sum value="+c);
+            c = a + b; //addition
+            Console.WriteLine("sum value=" + c);
             c = a - b; //subtraction
-            Console.WriteLine("subtraction value="+c);
+            Console.WriteLine("subtraction value=" + c);
             c = a * b; //multiplication
-            Console.WriteLine("multiplication="+c);
+            Console.WriteLine("multiplication=" + c);
             c = a % b; //modulus or remainder
             Console.WriteLine("remainder=" + c);
-            c= a / b; //divide
+            c = a / b; //divide
             Console.WriteLine("divide=" + c);
+            // Assignment operator (+=, -=,*=,/=,%=)                
+            // ex. += a=5, b=6
+            //  a+=b  
+            //  a=a+b a will be plus with b variable and is equal to value of a 
+            Console.WriteLine("enter value of c");
+            int e = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter value of d");
+            int d = int.Parse(Console.ReadLine());
+            e += d; //=+ used
+            Console.WriteLine("c after used +=:" + e);
+            e -= d; //-= used
+            Console.WriteLine("c after used -=: " + e);
+            e *= d; // *= used
+            Console.WriteLine("c after used *=:" + e);
+            e %= d;
+            Console.WriteLine("c after used %=:" + e);
+            Console.ReadKey();
 
         }
     }
