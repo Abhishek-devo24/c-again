14f8d4c3f611c4c3422d15b648321abbecf1cb9f
Abhishek-devo24 <guptaabhishek27538@gmail.com>
2026-03-16 16:06:46 +0530

    basic OOPs concept learn

---
+++ OOPs/OOPs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+class OOPs {
+    // i create a class name oops and another method name dis1 nd dis2 or main method.
+    //here i using object to call method which is different form like ststic nd dynamic.
+    //accessmodifier have 4 type public,private,internal,protected.
+    public static void dic1()
+    {
+        Console.WriteLine("static method");
+    }
+    public void dic2()
+    {
+        Console.WriteLine("dynamic method");
+    }
+    public static void Main(string[] args)
+    {
+       //OOPs run =new OOPs();
+       // run.dic2();
+        dic1();
+    }
+}
