12f085d874d2b5e90f9b50c816ead938432e061e
Abhishek-devo24 <guptaabhishek27538@gmail.com>
2026-03-17 15:08:32 +0530

    array and oops concept mix today work

--- OOPs/OOPs/Program.cs
+++ OOPs/OOPs/Program.cs
@@ -12,10 +12,43 @@ class OOPs {
     {
         Console.WriteLine("dynamic method");
     }
+    public void ascending() // dynamic method which is call by object in main method for run
+    {
+        int[] arr = new int[5];
+        for (int i=0;i<arr.Length;i++) {
+            arr[i] = int.Parse(Console.ReadLine());
+        
+        }
+        Array.Sort(arr);
+        Console.WriteLine("ascending order");
+        foreach (int r in arr)
+        {
+            Console.WriteLine(r);
+        }
+    }
+    public static void decending() // static method which is call by without object in main method
+    {
+        int[] array = new int[7];
+        for (int i=0;i<array.Length;i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        Array.Sort(array);
+        Array.Reverse(array);
+        Console.WriteLine("decending order");
+        foreach (int k in array)
+        {
+            Console.WriteLine(k);
+        }
+
+    }
     public static void Main(string[] args)
     {
-       //OOPs run =new OOPs();
-       // run.dic2();
-        dic1();
+       OOPs run =new OOPs();
+        //run.dic2();
+        // dic1();
+        decending();
+        Console.WriteLine("now turn of ascending order");
+        run.ascending();
     }
 }
